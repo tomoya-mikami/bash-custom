99e9be2989e16b4edbd98bf386b359a885e9801e
Tomoya Mikami <trymorecc@gmail.com>
2020-06-13 14:52:24 +0900

    load_library

--- common/common.bash
+++ common/common.bash
@@ -10,8 +10,5 @@ function add_line {
 }
 PROMPT_COMMAND='add_line'
 
-source $CUSTOM_SCRIPT_PATH/outsource_library/git/git-prompt.sh
-source $CUSTOM_SCRIPT_PATH/outsource_library/git/git-completion.bash
-
 export PS1=' \[\e[30;47m\] \t \[\e[37;46m\] \[\e[30m\] \W \[\e[36;49m\] \[\e[0m\] $(__git_ps1 "(%s)") \[\e[0m\] \$ '
 
---
+++ common/load_library.bash
@@ -0,0 +1,3 @@
+source $CUSTOM_SCRIPT_PATH/outsource_library/git/git-prompt.sh
+source $CUSTOM_SCRIPT_PATH/outsource_library/git/git-completion.bash
+
