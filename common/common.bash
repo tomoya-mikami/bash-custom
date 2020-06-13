fff192721284eaf44c5410ddb2c09bb980901a6e
Tomoya Mikami <trymorecc@gmail.com>
2020-06-13 16:43:48 +0900

    tmux

--- common/common.bash
+++ common/common.bash
@@ -12,3 +12,10 @@ PROMPT_COMMAND='add_line'
 
 export PS1=' \[\e[30;47m\] \t \[\e[37;46m\] \[\e[30m\] dir:\W \[\e[30;42m\] branch:$(__git_ps1 "(%s)") \[\e[0m\] \$ '
 
+# tmuxがインストールされていれば実行
+if which tmux >/dev/null 2>&1; then
+    #if not inside a tmux session, and if no session is started, start a new session
+    #もしtmuxの中にいないか起動していればサブシェルでアタッチ失敗したら新しいセッションを実行
+    test -z "$TMUX" && (tmux attach -t main || tmux new-session -s main)
+fi
+
